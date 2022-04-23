bf8e3403d37c823f91d9882e0e8bccd79a5fe1c6
miss-gif <ddc.akari@gmail.com>
2022-04-23 17:39:59 +0900

    FIX

--- js/main.js
+++ js/main.js
@@ -1,94 +1,11 @@
-// 사이드 퀵 메뉴 제어
-const sideSlide = document.querySelectorAll("ul.slide_number>li"); // 사이드 퀵 슬라이드 버튼
+new Swiper(".swiper", {
+  direction: "vertical", // 수직 슬라이드
+  spaceBetween: 30, // 슬라이드 사이 여백
+  mousewheel: true, // 마우스 휠 기능
 
-sideSlide[0].addEventListener("click", function () {
-  sideSlide[0].classList.add("onClick");
-  sideSlide[1].classList.remove("onClick");
-  sideSlide[2].classList.remove("onClick");
-  sideSlide[3].classList.remove("onClick");
-  sideSlide[4].classList.remove("onClick");
+  pagination: {
+    // 페이지 번호 사용 여부
+    el: ".swiper-pagination", // 페이지 번호 요소 선택자
+    clickable: true, // 페이지 버튼 활성화
+  },
 });
-
-sideSlide[1].addEventListener("click", function () {
-  sideSlide[0].classList.remove("onClick");
-  sideSlide[1].classList.add("onClick");
-  sideSlide[2].classList.remove("onClick");
-  sideSlide[3].classList.remove("onClick");
-  sideSlide[4].classList.remove("onClick");
-});
-
-sideSlide[2].addEventListener("click", function () {
-  sideSlide[0].classList.remove("onClick");
-  sideSlide[1].classList.remove("onClick");
-  sideSlide[2].classList.add("onClick");
-  sideSlide[3].classList.remove("onClick");
-  sideSlide[4].classList.remove("onClick");
-});
-
-sideSlide[3].addEventListener("click", function () {
-  sideSlide[0].classList.remove("onClick");
-  sideSlide[1].classList.remove("onClick");
-  sideSlide[2].classList.remove("onClick");
-  sideSlide[3].classList.add("onClick");
-  sideSlide[4].classList.remove("onClick");
-});
-
-sideSlide[4].addEventListener("click", function () {
-  sideSlide[0].classList.remove("onClick");
-  sideSlide[1].classList.remove("onClick");
-  sideSlide[2].classList.remove("onClick");
-  sideSlide[3].classList.remove("onClick");
-  sideSlide[4].classList.add("onClick");
-});
-
-// section1 메인화면
-
-// 배너 슬라이드 번호를 누르면 보이는 화면 제어
-const bannerNumber = document.querySelectorAll(".banner_number>li"); // 버튼 부분
-const articleNumber = document.querySelectorAll(".main_view>article"); // 이미지 부분
-
-bannerNumber[0].addEventListener("click", () => {
-  bannerNumber[0].style.opacity = "1";
-  bannerNumber[1].style.opacity = "0.3";
-  bannerNumber[2].style.opacity = "0.3";
-  bannerNumber[3].style.opacity = "0.3";
-  articleNumber[0].style.visibility = "visible";
-  articleNumber[1].style.visibility = "hidden";
-  articleNumber[2].style.visibility = "hidden";
-  articleNumber[3].style.visibility = "hidden";
-});
-
-bannerNumber[1].addEventListener("click", () => {
-  bannerNumber[0].style.opacity = "0.3";
-  bannerNumber[1].style.opacity = "1";
-  bannerNumber[2].style.opacity = "0.3";
-  bannerNumber[3].style.opacity = "0.3";
-  articleNumber[0].style.visibility = "hidden";
-  articleNumber[1].style.visibility = "visible";
-  articleNumber[2].style.visibility = "hidden";
-  articleNumber[3].style.visibility = "hidden";
-});
-
-bannerNumber[2].addEventListener("click", () => {
-  bannerNumber[0].style.opacity = "0.3";
-  bannerNumber[1].style.opacity = "0.3";
-  bannerNumber[2].style.opacity = "1";
-  bannerNumber[3].style.opacity = "0.3";
-  articleNumber[0].style.visibility = "hidden";
-  articleNumber[1].style.visibility = "hidden";
-  articleNumber[2].style.visibility = "visible";
-  articleNumber[3].style.visibility = "hidden";
-});
-
-bannerNumber[3].addEventListener("click", () => {
-  bannerNumber[0].style.opacity = "0.3";
-  bannerNumber[1].style.opacity = "0.3";
-  bannerNumber[2].style.opacity = "0.3";
-  bannerNumber[3].style.opacity = "1";
-  articleNumber[0].style.visibility = "hidden";
-  articleNumber[1].style.visibility = "hidden";
-  articleNumber[2].style.visibility = "hidden";
-  articleNumber[3].style.visibility = "visible";
-});
-
-// section4 프로젝트
